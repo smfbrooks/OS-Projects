433999e7763d77ba7a1c282d04d32c47458f2098
Stephen Brooks <smfbrooks@gmail.com>
2017-11-27 11:06:03 -0600

    c-scan finished.

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ bool contains(std::vector<int>* v, const int n) {
 
 void get_rands(std::vector<int>* v) {
     int num_ops = rand() % 51 + 25;
-    while(v->size() < 8) {
+    while(v->size() < num_ops) {
         int n = rand() % 200 + 1;
         if (!contains(v, n))
             v->push_back(n);
@@ -41,20 +41,6 @@ void align_pair(std::vector<int>* v, const int start) {
         v->emplace(v->begin() + start +1, v->at(pair_index));
         v->erase(v->begin() + pair_index + 1);
     }
-}
-
-
-int find_min(std::vector<int>* v) {
-    int min = std::numeric_limits<int>::max();
-    int min_index;
-
-    for(int i = 0; i < v->size(); i++) {
-        if (v->at(i) < min)
-            min = v->at(i);
-            min_index = i;
-    }
-
-    return min_index;
 }
 
 
@@ -91,7 +77,6 @@ int scan(std::vector<int>* v) {
         scan_v->push_back(v->at(i));
 
     while(scan_v->size() > 1) {
-        std::cout << "begin upward climb" << std::endl;
         for(int i = 1; !scan_v->empty() && i < scan_v->size(); i++) {
             if (scan_v->at(0) - scan_v->at(i) < 0) {
                 seek_amount += abs(scan_v->at(0) - scan_v->at(i));
@@ -100,12 +85,8 @@ int scan(std::vector<int>* v) {
                 scan_v->erase(scan_v->begin()+i);
                 i = 0;
             }
-            for(int j = 0; j < scan_v->size(); j++)
-                std::cout << scan_v->at(j) << "\t";
-            std::cout << std::endl;
         }
 
-        std::cout << "begin downward climb" << std::endl;
         for(int i = 1; !scan_v->empty() && i < scan_v->size(); i++) {
             if (scan_v->at(0) - scan_v->at(i) > 0) {
                 seek_amount += abs(scan_v->at(0) - scan_v->at(i));
@@ -114,12 +95,37 @@ int scan(std::vector<int>* v) {
                 scan_v->erase(scan_v->begin()+i);
                 i = 0;
             }
-            for(int j = 0; j < scan_v->size(); j++)
-                std::cout << scan_v->at(j) << "\t";
-            std::cout << std::endl;
         }
 
     }
+    return seek_amount;
+}
+
+
+int cscan(std::vector<int>* v) {
+    std::vector<int>* cscan_v = new std::vector<int>();
+    int seek_amount = 0;
+
+    for(int i = 0; i < v->size(); i++)
+        cscan_v->push_back(v->at(i));
+
+    int k = 0;
+    while(cscan_v->size() > 1) {
+        for(int i = 1; !cscan_v->empty() && i < cscan_v->size(); i++) {
+            if (cscan_v->at(0) - cscan_v->at(i) > 0) {
+                seek_amount += abs(cscan_v->at(0) - cscan_v->at(i));
+                cscan_v->erase(cscan_v->begin());
+                cscan_v->emplace(cscan_v->begin(), cscan_v->at(i-1));
+                cscan_v->erase(cscan_v->begin()+i);
+                i = 0;
+            }
+        }
+        if (cscan_v->size() > 1) {
+            seek_amount += abs(cscan_v->at(0) - cscan_v->at(1));
+            cscan_v->erase(cscan_v->begin());
+        }
+    }
+
     return seek_amount;
 }
 
@@ -128,18 +134,17 @@ int main () {
     std::vector<int>* rands_v = new std::vector<int>();
     std::srand((unsigned)time(NULL));
 
-    for(int i = 0; i < 1; i++) {
+    for(int i = 0; i < 10; i++) {
         rands_v->clear();
         get_rands(rands_v);
 
-        for(int j = 0; j < rands_v->size(); j++)
-            std::cout << rands_v->at(j) << "\t";
-        std::cout << std::endl;
+        // for(int j = 0; j < rands_v->size(); j++)
+        //     std::cout << rands_v->at(j) << "\t";
+        // std::cout << std::endl;
 
-        int seek_amount = scan(rands_v);
-
-        // std::cout << "Seeks for fcfs: " << fcfs(rands_v) << std::endl;
-        // std::cout << "Seeks for sstf: " << sstf(rands_v) << std::endl;
-        // std::cout << "Seeks for scan: " << scan(rands_v) << std::endl;
+        std::cout << "Seeks for fcfs: " << fcfs(rands_v) << std::endl;
+        std::cout << "Seeks for sstf: " << sstf(rands_v) << std::endl;
+        std::cout << "Seeks for scan: " << scan(rands_v) << std::endl;
+        std::cout << "Seeks for cscan: " << cscan(rands_v) << std::endl;
     }
 }
