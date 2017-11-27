8bd3e0cc9d8a37c6511cc799ed9f120c657676b9
Stephen Brooks <smfbrooks@gmail.com>
2017-11-26 21:26:56 -0600

    scan now working

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ bool contains(std::vector<int>* v, const int n) {
 
 void get_rands(std::vector<int>* v) {
     int num_ops = rand() % 51 + 25;
-    while(v->size() < num_ops) {
+    while(v->size() < 8) {
         int n = rand() % 200 + 1;
         if (!contains(v, n))
             v->push_back(n);
@@ -41,6 +41,20 @@ void align_pair(std::vector<int>* v, const int start) {
         v->emplace(v->begin() + start +1, v->at(pair_index));
         v->erase(v->begin() + pair_index + 1);
     }
+}
+
+
+int find_min(std::vector<int>* v) {
+    int min = std::numeric_limits<int>::max();
+    int min_index;
+
+    for(int i = 0; i < v->size(); i++) {
+        if (v->at(i) < min)
+            min = v->at(i);
+            min_index = i;
+    }
+
+    return min_index;
 }
 
 
@@ -68,20 +82,64 @@ int sstf(std::vector<int>* v) {
 }
 
 
+int scan(std::vector<int>* v) {
+    std::vector<int>* scan_v = new std::vector<int>();
+    int seek_amount = 0;
+    int pair_index;
+
+    for(int i = 0; i < v->size(); i++)
+        scan_v->push_back(v->at(i));
+
+    while(scan_v->size() > 1) {
+        std::cout << "begin upward climb" << std::endl;
+        for(int i = 1; !scan_v->empty() && i < scan_v->size(); i++) {
+            if (scan_v->at(0) - scan_v->at(i) < 0) {
+                seek_amount += abs(scan_v->at(0) - scan_v->at(i));
+                scan_v->erase(scan_v->begin());
+                scan_v->emplace(scan_v->begin(), scan_v->at(i-1));
+                scan_v->erase(scan_v->begin()+i);
+                i = 0;
+            }
+            for(int j = 0; j < scan_v->size(); j++)
+                std::cout << scan_v->at(j) << "\t";
+            std::cout << std::endl;
+        }
+
+        std::cout << "begin downward climb" << std::endl;
+        for(int i = 1; !scan_v->empty() && i < scan_v->size(); i++) {
+            if (scan_v->at(0) - scan_v->at(i) > 0) {
+                seek_amount += abs(scan_v->at(0) - scan_v->at(i));
+                scan_v->erase(scan_v->begin());
+                scan_v->emplace(scan_v->begin(), scan_v->at(i-1));
+                scan_v->erase(scan_v->begin()+i);
+                i = 0;
+            }
+            for(int j = 0; j < scan_v->size(); j++)
+                std::cout << scan_v->at(j) << "\t";
+            std::cout << std::endl;
+        }
+
+    }
+    return seek_amount;
+}
+
+
 int main () {
     std::vector<int>* rands_v = new std::vector<int>();
     std::srand((unsigned)time(NULL));
 
-    for(int i = 0; i < 10; i++) {
+    for(int i = 0; i < 1; i++) {
         rands_v->clear();
         get_rands(rands_v);
 
-        // for(int i = 0; i < rands_v->size(); i++)
-        //     std::cout << rands_v->at(i) << "\t";
-        // std::cout << std::endl;
+        for(int j = 0; j < rands_v->size(); j++)
+            std::cout << rands_v->at(j) << "\t";
+        std::cout << std::endl;
 
+        int seek_amount = scan(rands_v);
 
-        std::cout << "Seeks for fcfs: " << fcfs(rands_v) << std::endl;
-        std::cout << "Seeks for sstf: " << sstf(rands_v) << std::endl;
+        // std::cout << "Seeks for fcfs: " << fcfs(rands_v) << std::endl;
+        // std::cout << "Seeks for sstf: " << sstf(rands_v) << std::endl;
+        // std::cout << "Seeks for scan: " << scan(rands_v) << std::endl;
     }
 }
