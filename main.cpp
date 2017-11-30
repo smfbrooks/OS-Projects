8436c4a043293fde600d29e4ed43fe27a8005d5a
Stephen Brooks <smfbrooks@gmail.com>
2017-11-29 22:51:29 -0600

    scan, cscan corrections

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
@@ -5,7 +6,7 @@
 #include <vector>
 
 
-const int NUM_SIMS = 100;
+const int NUM_SIMS = 10;
 
 
 bool contains(std::vector<int>* v, const int n) {
@@ -18,17 +19,16 @@ bool contains(std::vector<int>* v, const int n) {
 }
 
 
-void get_rands(std::vector<int>* v) {
-    int num_ops = rand() % 51 + 25;
-    while(v->size() < num_ops) {
-        int n = rand() % 200 + 1;
+void get_rands(std::vector<int>* v, const int size) {
+    while(v->size() < size) {
+        int n = rand() % 199 + 1;
         if (!contains(v, n))
             v->push_back(n);
     }
 }
 
 
-void align_pair(std::vector<int>* v, const int start) {
+void align_pairs(std::vector<int>* v, const int start) {
     int distance = std::numeric_limits<int>::max();
     int pair_index;
     for(int i = start; i < v->size()-1; i++) {
@@ -44,6 +44,48 @@ void align_pair(std::vector<int>* v, const int start) {
 }
 
 
+void short_sort_dec(std::vector<int>* v) {
+    int pair_index;
+
+    for(int i = 0; i < v->size(); i++) {
+        int distance = std::numeric_limits<int>::max();
+        pair_index = i;
+        for(int j = i+1; j <= v->size()-1; j++) {
+            if (v->at(i) - v->at(j) > 0 && v->at(i) - v->at(j) < distance) {
+                distance = v->at(i) - v->at(j);
+                pair_index = j;
+            }
+        }
+        v->emplace(v->begin()+i+1, v->at(pair_index));
+        v->erase(v->begin()+pair_index+1);
+    }
+}
+
+
+void short_sort_asc(std::vector<int>* v) {
+    int pair_index;
+
+    for(int i = 0; i < v->size(); i++) {
+        int distance = std::numeric_limits<int>::max();
+        pair_index = i;
+        for(int j = i+1; j <= v->size()-1; j++) {
+            if (v->at(i) - v->at(j) < 0 && abs(v->at(i) - v->at(j)) < distance) {
+                distance = abs(v->at(i) - v->at(j));
+                pair_index = j;
+            }
+        }
+        v->emplace(v->begin()+i+1, v->at(pair_index));
+        v->erase(v->begin()+pair_index+1);
+    }
+}
+
+
+void full_sort_asc(std::vector<int>* v) {
+    std::sort(v->begin()+1, v->end());
+}
+
+
+
 int fcfs(std::vector<int>* v) {
     int seek_amount = 0;
     for(int i = 1; i < v->size(); i++)
@@ -54,76 +96,71 @@ int fcfs(std::vector<int>* v) {
 
 int sstf(std::vector<int>* v) {
     int seek_amount = 0;
-    std::vector<int>* sstf_v = new std::vector<int>();
-
-    for(int i = 0; i < v->size(); i++) {
-        sstf_v->push_back(v->at(i));
-    }
+    std::vector<int>* sstf_v = new std::vector<int>(*v);
 
     for(int i = 0; i < sstf_v->size(); i++) {
-        align_pair(sstf_v, i);
+        align_pairs(sstf_v, i);
     }
 
     return fcfs(sstf_v);
 }
 
 
-int scan(std::vector<int>* v) {
-    std::vector<int>* scan_v = new std::vector<int>();
+int scan_asc(std::vector<int>* v) {
     int seek_amount = 0;
-    int pair_index;
 
-    for(int i = 0; i < v->size(); i++)
-        scan_v->push_back(v->at(i));
-
-    while(scan_v->size() > 1) {
-        for(int i = 1; !scan_v->empty() && i < scan_v->size(); i++) {
-            if (scan_v->at(0) - scan_v->at(i) < 0) {
-                seek_amount += abs(scan_v->at(0) - scan_v->at(i));
-                scan_v->erase(scan_v->begin());
-                scan_v->emplace(scan_v->begin(), scan_v->at(i-1));
-                scan_v->erase(scan_v->begin()+i);
-                i = 0;
-            }
+    for(int i = 0; i < v->size(); i++) {
+        if (v->at(0) - v->at(i) < 0) {
+            seek_amount += abs(v->at(0) - v->at(i));
+            v->erase(v->begin());
+            i = 0;
         }
-
-        for(int i = 1; !scan_v->empty() && i < scan_v->size(); i++) {
-            if (scan_v->at(0) - scan_v->at(i) > 0) {
-                seek_amount += abs(scan_v->at(0) - scan_v->at(i));
-                scan_v->erase(scan_v->begin());
-                scan_v->emplace(scan_v->begin(), scan_v->at(i-1));
-                scan_v->erase(scan_v->begin()+i);
-                i = 0;
-            }
-        }
-
     }
+
     return seek_amount;
 }
 
 
-int cscan(std::vector<int>* v) {
-    std::vector<int>* cscan_v = new std::vector<int>();
+int scan_dec(std::vector<int>* v) {
+    int seek_amount = 0;
+    for(int i = 0; i < v->size(); i++) {
+        if (v->at(0) - v->at(i) > 0) {
+            seek_amount += abs(v->at(0) - v->at(i));
+            v->erase(v->begin());
+            i = 0;
+        }
+    }
+
+    return seek_amount;
+}
+
+
+int scan(std::vector<int>* vec) {
+    std::vector<int>* v = new std::vector<int>(*vec);
+
     int seek_amount = 0;
 
-    for(int i = 0; i < v->size(); i++)
-        cscan_v->push_back(v->at(i));
+    short_sort_asc(v);
+    seek_amount += scan_asc(v);
+    short_sort_dec(v);
+    seek_amount += scan_dec(v);
 
-    int k = 0;
-    while(cscan_v->size() > 1) {
-        for(int i = 1; !cscan_v->empty() && i < cscan_v->size(); i++) {
-            if (cscan_v->at(0) - cscan_v->at(i) > 0) {
-                seek_amount += abs(cscan_v->at(0) - cscan_v->at(i));
-                cscan_v->erase(cscan_v->begin());
-                cscan_v->emplace(cscan_v->begin(), cscan_v->at(i-1));
-                cscan_v->erase(cscan_v->begin()+i);
-                i = 0;
-            }
-        }
-        if (cscan_v->size() > 1) {
-            seek_amount += abs(cscan_v->at(0) - cscan_v->at(1));
-            cscan_v->erase(cscan_v->begin());
-        }
+    return seek_amount;
+}
+
+
+int cscan(std::vector<int>* vec) {
+    std::vector<int>* v = new std::vector<int>(*vec);
+
+
+    int seek_amount = 0;
+    short_sort_asc(v);
+    seek_amount += scan_asc(v);
+    full_sort_asc(v);
+    if (v->size() > 1) {
+        seek_amount += abs(v->at(0) - v->at(1));
+        v->erase(v->begin());
+        seek_amount += scan_asc(v);
     }
 
     return seek_amount;
@@ -133,18 +170,21 @@ int cscan(std::vector<int>* v) {
 int main () {
     std::vector<int>* rands_v = new std::vector<int>();
     std::srand((unsigned)time(NULL));
+    long fcfs_seeks, sstf_seeks, scan_seeks, cscan_seeks;
+    fcfs_seeks = sstf_seeks = scan_seeks = cscan_seeks = 0;
 
-    for(int i = 0; i < 10; i++) {
-        rands_v->clear();
-        get_rands(rands_v);
+    for(int i = 0; i < NUM_SIMS; i++) {
+        for(int j = 25; j < 26; j++) {
+            rands_v->clear();
+            get_rands(rands_v, j);
+            
+            for(int k = 0; k < rands_v->size(); k++)
+                std::cout << rands_v->at(k) << "\t";
+            std::cout << std::endl;
 
-        // for(int j = 0; j < rands_v->size(); j++)
-        //     std::cout << rands_v->at(j) << "\t";
-        // std::cout << std::endl;
+            std::cout << "fcfs: " << fcfs(rands_v) << "\nsstf: " << sstf(rands_v) <<
+                "\nscan: " << scan(rands_v) << "\ncscan: " << cscan(rands_v) << std::endl;
+        }
 
-        std::cout << "Seeks for fcfs: " << fcfs(rands_v) << std::endl;
-        std::cout << "Seeks for sstf: " << sstf(rands_v) << std::endl;
-        std::cout << "Seeks for scan: " << scan(rands_v) << std::endl;
-        std::cout << "Seeks for cscan: " << cscan(rands_v) << std::endl;
     }
 }
